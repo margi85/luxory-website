3629a24ac80b3278f363dd6959f959469d5e6130
Margarita Krusharska <margarita.krusharska@gmail.com>
2019-08-01 21:50:02 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let gulp = require("gulp"),
+	autoprefixer = require("gulp-autoprefixer"),
+	exec = require("gulp-exec"),
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass'),
+	cp = require("child_process");
+
+gulp.task("css", function() {
+	return gulp.src( '_assets/css/**/*.css' )
+		// .pipe( sass().on('error', sass.logError) )
+		.pipe( autoprefixer() )
+		.pipe( gulp.dest( './docs/css/' ) )
+		.pipe( browserSync.stream({ match: '**/*.css' }) )
+	;
+});
+
+// Jekyll
+gulp.task("jekyll", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
+});
+
+gulp.task("watch", function() {
+
+	browserSync.init({
+		server: {
+            baseDir: "./docs/"
+    }
+	});
+
+	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+
+	gulp.watch(
+		[
+			"./*.html",
+			"./_includes/*.html",
+			"./_layouts/*.html",
+			"./_posts/**/*.*"
+		]
+	).on('change', gulp.series('jekyll') );
+
+  const reloadLater = () => setTimeout(browserSync.reload, 0);
+
+  gulp.watch(['docs/**/*.html', 'docs/**/*.js']).on('change', reloadLater );
+});
+
+gulp.task("deploy", gulp.series('jekyll', 'css'));
+
+gulp.task("default", gulp.series('jekyll', 'css', 'watch'));
